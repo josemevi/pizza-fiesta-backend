4bf20ac3c3c548a2e5d689ed7380192497a726cb
José David Melián Villalobos <josedavidmelian@gmail.com>
2021-07-14 22:41:20 +0100

    index fix

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
-<h1>Hola, esto es solamente una api asi que chao.</h1>
+<h1>Hola, esto es solamente una api asi que bueno, no se que haces aquí o que esperabas.</h1>
+
+<h1>Link del front: <a href="https://josemevi.github.io/pizza-fiesta-frontend/login">Aquí</a></h1>
